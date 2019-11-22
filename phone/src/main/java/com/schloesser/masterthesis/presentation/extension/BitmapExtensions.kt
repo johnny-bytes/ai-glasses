ed8f6633064b2400c0591ff26a39e74a1b431bd8
Timo Schlößer <timo.schloesser@gmail.com>
2019-11-22 18:15:21 +0100

    added OpenCV library

---
+++ phone/src/main/java/com/schloesser/masterthesis/presentation/extension/BitmapExtensions.kt
@@ -0,0 +1,14 @@
+package com.schloesser.masterthesis.presentation.extension
+
+import android.graphics.Bitmap
+import org.opencv.android.Utils
+import org.opencv.core.Mat
+
+/**
+ * Extension functions for [Bitmap].
+ */
+fun Bitmap.toMat() : Mat {
+    val mat = Mat()
+    Utils.bitmapToMat(this, mat)
+    return mat
+}
